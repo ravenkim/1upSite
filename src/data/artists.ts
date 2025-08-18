67de27542e7ada2df6bdc14259b9a3dd6015fbbd
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-08-18 13:23:03 +0900

    아티스트 정보 수정

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -5,107 +5,100 @@ export interface Artist {
         en: string
         ko: string
     }
-    bio: {
-        en: string
-        ko: string
-    }
+
+    instaId:string,
+    instaUrl:string,
     imageUrl: string
 }
 
 export const artists: Artist[] = [
     {
         id: '1',
-        name: 'popsick',
+        name: 'GOLDBUUDA',
         profession: {
-            en: 'Musician',
-            ko: '음악가',
+            en: 'Founder of 1UP, Executive Director, Artist, Producer',
+            ko: '1UP 창립자, 총괄 디렉터, 아티스트, 프로듀서',
         },
-        bio: {
-            en: 'Artist A is a talented musician known for their unique sound, blending traditional melodies with modern electronic beats. Their live performances are a captivating experience, drawing audiences into a world of rhythm and emotion.',
-            ko: '아티스트 A는 전통적인 멜로디와 현대적인 일렉트로닉 비트를 혼합한 독특한 사운드로 유명한 재능 있는 음악가입니다. 그들의 라이브 공연은 리듬과 감정의 세계로 관객을 끌어들이는 매혹적인 경험입니다.',
-        },
-        imageUrl:
-            'https://search.pstatic.net/common?type=b&size=216&expire=1&refresh=true&quality=100&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2F166%2F202008191553505781.jpg',
+        instaId: 'goldbuuda',
+        instaUrl: 'https://www.instagram.com/goldbuuda/',
+        imageUrl: '',
     },
     {
         id: '2',
-        name: 'Artist B',
+        name: 'Gh0st',
         profession: {
-            en: 'Digital Artist',
-            ko: '디지털 아티스트',
+            en: 'DJ, Creative Director, Management',
+            ko: '디제이, 크리에이티브 디렉터, 매니지먼트',
         },
-        bio: {
-            en: 'Artist B specializes in digital art and captivating visuals, creating intricate and vibrant pieces that explore themes of nature and technology. Their work often features surreal landscapes and futuristic cityscapes.',
-            ko: '아티스트 B는 디지털 아트와 매혹적인 비주얼을 전문으로 하며, 자연과 기술의 테마를 탐구하는 복잡하고 생동감 넘치는 작품을 만듭니다. 그들의 작품은 종종 초현실적인 풍경과 미래적인 도시 풍경을 특징으로 합니다.',
-        },
-        imageUrl: '/src/assets/images/main2.jpg',
+        instaId: 'gh0st__x__',
+        instaUrl: 'https://www.instagram.com/gh0st__x__/',
+        imageUrl: '',
     },
     {
         id: '3',
-        name: 'Artist C',
+        name: 'POPXICK',
         profession: {
-            en: 'Sculptor',
-            ko: '조각가',
+            en: 'Artist, Producer, Mixing Engineer, Operations Lead at 1UP',
+            ko: '아티스트, 프로듀서, 믹싱 엔지니어, 1UP 운영 리드',
         },
-        bio: {
-            en: 'Artist C is a renowned sculptor, creating intricate pieces from various materials including metal, wood, and stone. Their sculptures often depict abstract forms and human figures, conveying deep philosophical messages.',
-            ko: '아티스트 C는 금속, 나무, 돌을 포함한 다양한 재료로 복잡한 작품을 만드는 유명한 조각가입니다. 그들의 조각품은 종종 추상적인 형태와 인물을 묘사하며 깊은 철학적 메시지를 전달합니다.',
-        },
-        imageUrl: '/src/assets/images/main2.jpg',
+        instaId: 'popxick',
+        instaUrl: 'https://www.instagram.com/popxick/',
+        imageUrl: '',
     },
     {
         id: '4',
-        name: 'Artist C',
+        name: 'SUJIN LIM',
         profession: {
-            en: 'Sculptor',
-            ko: '조각가',
+            en: 'Model, Video Editor',
+            ko: '모델, 비디오 에디터',
         },
-        bio: {
-            en: 'Artist C is a renowned sculptor, creating intricate pieces from various materials including metal, wood, and stone. Their sculptures often depict abstract forms and human figures, conveying deep philosophical messages.',
-            ko: '아티스트 C는 금속, 나무, 돌을 포함한 다양한 재료로 복잡한 작품을 만드는 유명한 조각가입니다. 그들의 조각품은 종종 추상적인 형태와 인물을 묘사하며 깊은 철학적 메시지를 전달합니다.',
-        },
-        imageUrl: '/src/assets/images/main2.jpg',
+        instaId: 'sujinymph',
+        instaUrl: 'https://www.instagram.com/sujinymph/',
+        imageUrl: '',
     },
     {
-        id: '4',
-        name: 'Artist C',
+        id: '5',
+        name: 'JP',
         profession: {
-            en: 'Sculptor',
-            ko: '조각가',
+            en: 'Photographer, Video Director',
+            ko: '포토그래퍼, 영상 디렉터',
         },
-        bio: {
-            en: 'Artist C is a renowned sculptor, creating intricate pieces from various materials including metal, wood, and stone. Their sculptures often depict abstract forms and human figures, conveying deep philosophical messages.',
-            ko: '아티스트 C는 금속, 나무, 돌을 포함한 다양한 재료로 복잡한 작품을 만드는 유명한 조각가입니다. 그들의 조각품은 종종 추상적인 형태와 인물을 묘사하며 깊은 철학적 메시지를 전달합니다.',
-        },
-        imageUrl: '/src/assets/images/main2.jpg',
+        instaId: 'jpp_.17',
+        instaUrl: 'https://www.instagram.com/jpp_.17/',
+        imageUrl: '',
     },
-
     {
-        id: '4',
-        name: 'Artist C',
+        id: '6',
+        name: 'Skelly',
         profession: {
-            en: 'Sculptor',
-            ko: '조각가',
+            en: 'Artist, Producer',
+            ko: '아티스트, 프로듀서',
         },
-        bio: {
-            en: 'Artist C is a renowned sculptor, creating intricate pieces from various materials including metal, wood, and stone. Their sculptures often depict abstract forms and human figures, conveying deep philosophical messages.',
-            ko: '아티스트 C는 금속, 나무, 돌을 포함한 다양한 재료로 복잡한 작품을 만드는 유명한 조각가입니다. 그들의 조각품은 종종 추상적인 형태와 인물을 묘사하며 깊은 철학적 메시지를 전달합니다.',
-        },
-        imageUrl: '/src/assets/images/main2.jpg',
+        instaId: 'skeletontheblock',
+        instaUrl: 'https://www.instagram.com/skeletontheblock/',
+        imageUrl: '',
     },
-
     {
-        id: '4',
-        name: 'Artist C',
+        id: '7',
+        name: 'chilloud',
         profession: {
-            en: 'Sculptor',
-            ko: '조각가',
+            en: 'Producer, Artist, DJ, Tech Operator',
+            ko: '프로듀서, 아티스트, DJ, 테크 오퍼레이터',
         },
-        bio: {
-            en: 'Artist C is a renowned sculptor, creating intricate pieces from various materials including metal, wood, and stone. Their sculptures often depict abstract forms and human figures, conveying deep philosophical messages.',
-            ko: '아티스트 C는 금속, 나무, 돌을 포함한 다양한 재료로 복잡한 작품을 만드는 유명한 조각가입니다. 그들의 조각품은 종종 추상적인 형태와 인물을 묘사하며 깊은 철학적 메시지를 전달합니다.',
+        instaId: 'chilloudx',
+        instaUrl: 'https://www.instagram.com/chilloudx/',
+        imageUrl: '',
+    },
+    {
+        id: '8',
+        name: 'Rodi',
+        profession: {
+            en: 'Dancer',
+            ko: '댄서',
         },
-        imageUrl: '/src/assets/images/main2.jpg',
+        instaId: '_iamrodi',
+        instaUrl: 'https://www.instagram.com/_iamrodi/',
+        imageUrl: '',
     },
 ]
 
